a3fe3e8e22ca6b0ab6105771da97a4556e8308ec
dasd <35190710+mrigankahazarika@users.noreply.github.com>
2023-08-04 18:03:53 +0530

    Update index.js
    fixed conflict

--- index.js
+++ index.js
@@ -85,13 +85,13 @@ app.get('/callback/', async (req, res) => {
 	}
 	res.redirect(`https://jivimart.com/Confirm/${responseData.payment_status + '_' + responseData.payment_id}`)
 	console.log(req, 'ooo')
-<<<<<<< HEAD
-	console.log(req.body, 'ooobody')
-	console.log(res.body, 'ressbody')
+// <<<<<<< HEAD
+	// console.log(req.body, 'ooobody')
+	// console.log(res.body, 'ressbody')
 	// console.log(req.cookies.s_l, 'ooo2')
-=======
-	console.log(req, 'ooo2')
->>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
+// =======
+	// console.log(req, 'ooo2')
+// >>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
 	// console.log(res, 'ppp')
 
 
@@ -107,7 +107,7 @@ app.get('/callback/', async (req, res) => {
 	// let orderidcookie = (req.cookies.ordercookie)
 	// console.log("orderidcookie777", orderidcookie)
 	// console.log('newCookies2', tokenn)
-<<<<<<< HEAD
+// <<<<<<< HEAD
 	// let url_parts = url.parse(req.url, true),
 	// 	responseData = url_parts.query;
 	// console.log('responseData callback', responseData)
@@ -116,8 +116,8 @@ app.get('/callback/', async (req, res) => {
 	// 	payment_id: responseData.payment_id,
 	// 	id: req.cookies.ordercookie
 	// }
-=======
->>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
+// =======
+// >>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
 	// console.log(data, 'datadatadata')
 	// console.log(responseData, 'poopop')
 
@@ -133,11 +133,11 @@ app.get('/callback/', async (req, res) => {
 	// 	try {
 	// 		let orderdata = axios.defaults.headers.common['Authorization'] = `Bearer ${tokenn}`;
 	// 		console.log('llll', orderdata)
-<<<<<<< HEAD
+// <<<<<<< HEAD
 	// 		const response = axios.post('https://super.jivimart.com/public/api/user/order/make/payment/id', data)
-=======
+// =======
 	// 		const response = axios.post('https://jivimart.com/public/api/user/order/make/payment/id', data)
->>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
+// >>>>>>> b47d243ffee639ec2f1f1ae5fbfac2390fe9fa00
 	// 			.then(ress => {
 	// 				console.log("postdata", ress)
 	// 				let sendorderid = ress.data.data.order_uuid
